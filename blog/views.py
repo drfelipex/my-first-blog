0b9065ae2fcd3635de6b9aaa4aa17e128307d8f9
drfelipex <drfelipex1987@gmail.com>
2017-01-26 19:54:24 +0000

    Create/edit blog post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from .models import Post
 
 
@@ -11,4 +11,3 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-
